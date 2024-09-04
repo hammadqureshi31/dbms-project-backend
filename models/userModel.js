480f45f741dde3c324981f5d0efb6374a83851e3
hammadqureshi31 <hammad2004qureshi@gmail.com>
2024-09-05 00:59:39 +0500

    google auth error fix

--- models/userModel.js
+++ models/userModel.js
@@ -14,8 +14,6 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        required: true,
-        unique: true,
     },
     profilePicture: {
         type: String,
